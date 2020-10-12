43c0894b7b6b91d70aa20d06c8a7078502099fca
RetroPronghorn <retropronghorn@gmail.com>
2020-10-12 19:11:05 -0400

    test

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+document.title = 'RETROPRONGHORN';
+setInterval(() => {
+    document.title = 'RETROPRONGHORN';
+    setTimeout(() => {
+        document.title = 'R3TR0PR0NGH04N';
+    }, 200);
+    setTimeout(() => {
+        document.title = 'RETROPRONGHORN';
+    }, 500);
+    setTimeout(() => {
+        document.title = 'R3TR0PR0NGH04N';
+    }, 800);
+    setTimeout(() => {
+        document.title = 'RETR0PRRRGHORN';
+    }, 900);
+    setTimeout(() => {
+        document.title = 'RET-HIRE_ME-GHN';
+    }, 1000);
+    setTimeout(() => {
+        document.title = 'RETROPRONGHORN';
+    }, 1200);
+}, 3000);
